81a1f3120335f288023fd8bcaa624a40e29d862f
ararita <munjizavesna@gmail.com>
2020-07-07 10:05:02 +0200

    First commit

---
+++ server/index.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
+
+require("dotenv").config();
+console.log("************");
+console.log(process.env);
+console.log("************");
+//middleware
+app.use(cors());
+app.use(express.json()); //req.body
+
+//ROUTES
+
+//create todo
+app.post("/todos", async (req, res) => {
+  try {
+    // console.log("req.body", req.body);
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//get a todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id=($1)", [
+      id,
+    ]);
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//update a todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const { id } = req.params;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+    res.json(updateTodo);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//delete a todo
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json(deleteTodo);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+app.listen(5000, () => {
+  console.log("listening on port 5000");
+});
